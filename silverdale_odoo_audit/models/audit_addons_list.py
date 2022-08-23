c7749d87e9251f15f590db132948421bda6e10c3
Muzher <muzher@silverdaletech.com>
2022-08-23 18:02:27 +0500

    Removed SME modules outside the directory

--- silverdale_odoo_audit/models/audit_addons_list.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-
-class AuditAddonsList(models.Model):
-    _name = 'audit.addons.list'
-    _description = 'Audit Addons List'
-
-    name = fields.Char('Name', required=1)
--- silverdale_odoo_audit/wizard/__init__.py
+++
@@ -1,3 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from . import audit_addons_list_wizard
