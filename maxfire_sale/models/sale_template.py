e4e3dceb9adfd90876cb4b84f3e3f42dc179bc1f
Silverdale-Obaid <obaid.ahmed@silverdaletech.com>
2024-08-02 09:26:40 -0700

    remove csd+sme

--- maxfire_sale/models/sale_template.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models
-
-
-class SaleOrderTemplate(models.Model):
-    _inherit = 'sale.order.template'
-
-    lines_note = fields.Html('Note')
--- printnode_base/static/src/js/constants.js
+++
@@ -1,7 +0,0 @@
-/** @odoo-module **/
-
-export default WORKSTATION_DEVICES = [
-    'printnode_workstation_printer_id',
-    'printnode_workstation_label_printer_id',
-    'printnode_workstation_scales_id'
-];
